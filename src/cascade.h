4b86bfc72a925bff724040d79e939fecfb124e80
Solant <runner62v6@gmail.com>
2018-05-25 16:56:45 +0000

    Moved everything into single header file

--- src/cascade.h
+++ src/cascade.h
@@ -4,16 +4,17 @@
 #include <QJsonObject>
 #include <QMetaObject>
 #include <QMetaProperty>
-#include "cascade_global.h"
+#include <QDebug>
 
-class CASCADESHARED_EXPORT Cascade
+class Cascade
 {
 
 public:
     Cascade();
 
     template <typename T>
-    static T from(const QJsonObject &obj) {
+    static T from(const QJsonObject &obj)
+    {
         T result = T();
         const QMetaObject metaObject = T::staticMetaObject;
         from(&result, obj, metaObject);
@@ -21,7 +22,24 @@ public:
     }
 
 private:
-    static void from(void *target, const QJsonObject &obj, const QMetaObject &targetMetaObject);
+    static void from(void *target, const QJsonObject &obj, const QMetaObject &targetMetaObject)
+    {
+        int props = targetMetaObject.propertyCount();
+        for (int i = 0; i < props; i++) {
+            const auto &prop = targetMetaObject.property(i);
+            const auto &name = prop.name();
+            if (obj.contains(name)) {
+                const QJsonValue &value = obj.value(name);
+                if (
+                    value.isArray()
+                    || value.isObject()
+                ) {
+                    qWarning() << "Nested object deserialization is not supported";
+                }
+                prop.writeOnGadget(target, value.toVariant());
+            }
+        }
+    }
 };
 
 #endif // CASCADE_H
--- test/test.qbs
+++ test/test.qbs
@@ -3,12 +3,10 @@ import qbs
 Project {
     CppApplication {
         Depends { name: "Qt.testlib" }
-	Depends { name: "cascade" }
         consoleApplication: true
         files: [
             "tst_testtest.cpp",
             "plaingadget.h",
         ]
     }
-    references: ["../cascade.qbs"]
 }
